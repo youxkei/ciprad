98e173f63f1074fd331c9dff2ac0b2abe0b8c837
unknown <youkei@youkei-PC.(none)>
2012-02-17 15:39:09 +0900

    added test code

--- src/ciprad.d
+++ src/ciprad.d
@@ -1,4 +1,28 @@
 module ciprad;
+version(all){
+    template temp(T){
+        void temp(U)(U u){
+            import std.stdio;writeln(u);
+        }
+    }
+    void main(){
+        temp!int("hello");
+    }
+}
+
+version(none){
+    import std.typecons;
+    void main(){
+        bool[Tuple!(int, int, string)] aa;
+        aa[tuple(1, 1, "hello")] = true;
+        assert(tuple(1, 1, "hello") in aa);
+    }
+
+    static assert({
+        main();
+        return true;
+    }());
+}
 
 version(all){
     struct S{
